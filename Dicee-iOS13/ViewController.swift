819448f73858bdaa6477ff262de6623d10490a5a
shinohara <taisei.shinohara1329@gmail.com>
2024-07-24 12:29:21 +0900

    chore: diceImageの接続を確認

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceImageViewOne: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        diceImageViewOne.image = UIImage(imageLiteralResourceName: "DiceSix")
     }
 
 
